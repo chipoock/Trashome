d90d580395ffe126a0f4e51d4aeaa9fcbb79851d
Josshua David <2shuadavi@gmail.com>
2026-05-13 19:52:57 -0600

    Creacion Vista y controlador para Registro de Empleados

---
+++ src/application/model/Conductor.java
@@ -0,0 +1,26 @@
+package application.model;
+
+import java.time.LocalDate;
+
+public class Conductor extends Employee {
+	private Camiones camionAsignado;
+
+	public Conductor(String name, int age, int idUser, int CP, String email, String phone, String password, String address,
+			LocalDate hiringDate, int salary, Camiones camionAsignado) {
+		super(name, age, idUser, CP, email, phone, password, address, hiringDate, salary);
+        
+
+        setCamionAsignado(camionAsignado);
+	}
+
+	public Camiones getCamionAsignado() {
+		return camionAsignado;
+	}
+
+	public void setCamionAsignado(Camiones camionAsignado) {
+		if(camionAsignado == null) {
+			throw new IllegalArgumentException("Todos los conductores deben tener un camion asignado");
+		}
+		this.camionAsignado = camionAsignado;
+	}
+}
